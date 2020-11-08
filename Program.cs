1c412af0205fb1b85c62ab4f4a0bac59f69edaaa
Michal Nadstawny <michnadstawny@gmail.com>
2020-11-08 13:40:41 +0100

    WorkingWithIntegers(), OrderPrecedence(), int.MaxValue, int.MinValue

--- Program.cs
+++ Program.cs
@@ -4,12 +4,59 @@ namespace NumbersInCSharp
 {
     class Program
     {
-        static void Main(string[] args)
+        static void WorkingWithIntegers()
         {
             int a = 18;
             int b = 6;
+
+            // addition
             int c = a + b;
             Console.WriteLine(c);
+
+            // subtraction
+            c = a - b;
+            Console.WriteLine(c);
+
+            // multiplication
+            c = a * b;
+            Console.WriteLine(c);
+
+            // division
+            c = a / b;
+            Console.WriteLine(c);
+        }
+
+        static void OrderPrecedence()
+        {
+            int a = 5;
+            int b = 4;
+            int c = 2;
+            int d = a + b * c;
+            Console.WriteLine(d);
+
+            d = (a + b) * c;
+            Console.WriteLine(d);
+
+            d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+            Console.WriteLine(d);
+
+            int e = 7;
+            int f = 4;
+            int g = 3;
+            int h = (e  + f) / g;
+            Console.WriteLine(h);
+        }
+
+        static void Main(string[] args)
+        {
+            WorkingWithIntegers();
+
+            OrderPrecedence();
+
+            int max = int.MaxValue;
+            int min = int.MinValue;
+            Console.WriteLine($"The range of integers is {min} to {max}");
+
         }
     }
 }
