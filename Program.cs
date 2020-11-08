8f40020e5cbd20da45fff6a68bc259ed5db03048
Michal Nadstawny <michnadstawny@gmail.com>
2020-11-08 16:04:30 +0100

    challenge 2
    radius()

--- Program.cs
+++ Program.cs
@@ -4,25 +4,18 @@ namespace NumbersInCSharp
 {
     class Program
     {
-        static void WorkWithDoubles()
+        static void radius()
         {
-            double a = 5;
-            double b = 4;
-            double c = 2;
-            double d = 100;
-            double e = 19;
-            double f = 23;
-            double g = 8;
-            double h = 144;
-            double wynik1 = d / a + (b * c) - (e - f) * (h - g);
-            double wynik2 = g * (d * f / c) - a * (b + e) - h;
-            Console.WriteLine(wynik1);
-            Console.WriteLine(wynik2);
+            double r = 2.50;
+
+            double result = Math.PI * Math.Pow(r, 2);
+
+            Console.WriteLine(result);
         }
 
         static void Main(string[] args)
         {
-            WorkWithDoubles();
+            radius();
         }
     }
 }
